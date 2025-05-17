f2873f6e824c9382cd894dd64de4645af9343751
Carlos Limão <carlos.limao@bitstuff.pt>
2025-05-17 14:00:26 +0100

    Melhorias na página principal.

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,138 +1,102 @@
 <!-- src/views/HomePage.vue -->
 <template>
-  <section class="max-w-7xl mx-auto text-center py-20">
-    <h2 class="text-4xl font-bold text-blue-600 mb-4">Available Jobs</h2>
+  <section class="max-w-7xl mx-auto text-center py-4">
+    <h2 class="text-4xl font-bold text-offgreen-700 mb-8">Available Jobs</h2>
 
-    <input
-      v-model="searchQuery"
-      type="text"
-      placeholder="Search jobs..."
-      class="border border-gray-300 rounded-lg p-2 mb-4 w-full max-w-md mx-auto"
-    />
+    <div class="flex flex-row items-center justify-between mb-8">
+      <input
+        v-model="searchQuery"
+        type="text"
+        placeholder="Search jobs..."
+        class="border border-gray-300 rounded-lg p-2 w-full max-w-md placeholder:text-offgreen-dark/50"
+      />
 
-    <!-- Filtros -->
-    <label for="location" class="text-gray-700 mb-2 px-2">Location:</label>
-    <select v-model="selectedLocation" class="border border-gray-300 rounded-lg p-2">
-      <option value="">All Locations</option>
-      <optgroup label="North America">
-        <option>Canada</option>
-        <option>Mexico</option>
-        <option>Puerto Rico</option>
-        <option>USA</option>
-      </optgroup>
-      <optgroup label="South America">
-        <option>Argentina</option>
-        <option>Brazil</option>
-        <option>Chile</option>
-        <option>Colombia</option>
-        <option>Peru</option>
-      </optgroup>
-      <optgroup label="Europe">
-        <option>Austria</option>
-        <option>Belgium</option>
-        <option>Denmark</option>
-        <option>Finland</option>
-        <option>France</option>
-        <option>Germany</option>
-        <option>Hungary</option>
-        <option>Ireland</option>
-        <option>Italy</option>
-        <option>Netherlands</option>
-        <option>Norway</option>
-        <option>Poland</option>
-        <option>Portugal</option>
-        <option>Spain</option>
-        <option>Sweden</option>
-        <option>Switzerland</option>
-        <option>Russia</option>
-        <option>UK</option>
-        <option>Ukraine</option>
-      </optgroup>
-      <optgroup label="Asia">
-        <option>China</option>
-        <option>Hong Kong</option>
-        <option>India</option>
-        <option>Indonesia</option>
-        <option>Japan</option>
-        <option>Malaysia</option>
-        <option>Philippines</option>
-        <option>Singapore</option>
-        <option>South Korea</option>
-        <option>Thailand</option>
-        <option>Vietnam</option>
-      </optgroup>
-      <optgroup label="Oceania">
-        <option>Australia</option>
-        <option>New Zealand</option>
-      </optgroup>
-      <optgroup label="Africa">
-        <option>Egypt</option>
-        <option>South Africa</option>
-      </optgroup>
-    </select>
+      <!-- This CountrySelect component can be used anywhere we need a country -->
+      <CountrySelect v-model="selectedLocation"
+                    :aliases="{ 'United Kingdom': 'UK', 'United States': 'USA' }"
+                    label="Location:"
+                    labelClass="text-gray-700 px-6"
+                    selectClass="border border-gray-300 rounded-lg p-2"
+                    wrapperClass="inline"
+                    placeholder="Select a country"
+                    placeholderClass="text-offgreen-dark/50"
+                    optionClass="text-gray-700"
+                    />
 
-    <label for="salary" class="text-gray-700 mb-2 px-2">Salary:</label>
-    <select v-model="selectedSalaryRange" class="border border-gray-300 rounded-lg p-2">
-      <option value="">All Salaries</option>
-      <option value="0-50000">0 - 50,000</option>
-      <option value="50001-100000">50,001 - 100,000</option>
-      <option value="100001-150000">100,001 - 150,000</option>
-      <option value="150001-200000">150,001 - 200,000</option>
-    </select>
+      <div>
+        <label for="selectedSalaryRange" class="text-gray-700 px-6">Salary:</label>
+        <select v-model="selectedSalaryRange" class="border border-gray-300 rounded-lg p-2 text-offgreen-dark/50">
+          <option class="text-gray-700" value="">All Salaries</option>
+          <option class="text-gray-700" value="0-50000">0 - 50,000</option>
+          <option class="text-gray-700" value="50001-100000">50,001 - 100,000</option>
+          <option class="text-gray-700" value="100001-150000">100,001 - 150,000</option>
+          <option class="text-gray-700" value="150001-200000">150,001 - 200,000</option>
+        </select>
+        </div>
+    </div>
 
     <div v-if="loading">Loading data...</div>
     <div v-else-if="error">Error loading data: {{ error }}</div>
     <div v-else>
-      <h1>{{ filteredJobs.length }}</h1>
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10">
-        <div
-          v-for="item in filteredJobs"
-          :key="item.id"
-          class="rounded overflow-hidden shadow-lg flex flex-col"
-        >
-          <a :href="`/job/${item.id}`">
-            <div class="relative flex flex-row">
-              <div>
-                <img
-                  class="min-w-[100px]"
-                  :src="item.company_logo"
-                  :alt="item.company_name"
-                  :title="item.company_name"
-                  width="100px"
-                />
+      <h1>Showing {{ filteredJobs.length }} results</h1>
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10 items-stretch">
+        <div v-for="job in filteredJobs" :key="job.id" class="flex flex-col bg-white rounded-lg shadow-md p-3 mb-4 hover:shadow-lg transition-shadow duration-300 relative">
+          <div class="flex flex-col h-full">
+            <div class="flex items-start space-x-4">
+
+              <div class="flex-1">
+                <!-- Job Icon -->
+                <img :src="job.company_logo" :alt="job.company_name" :title="job.company_name" class="w-12 h-12 rounded-md object-contain" />
+
+                <!-- Save button -->
+                <button
+                  @click.stop="toggleSave(job.id)"
+                  :class="[
+                    'float-left ms-4 mt-4 text-gray-400 cursor-pointer',
+                    isSaved(job.id) ? 'text-green-500' : 'text-red-500',
+                    isSaved(job.id) ? 'hover:text-red-500' : 'hover:text-green-500'
+                  ]"
+                  :aria-label="isSaved(job.id) ? 'Unsave job' : 'Save job'"
+                >
+                  <ThumbsUp :size="16" />
+                </button>
               </div>
-              <div class="flex flex-col w-full items-center mx-1 my-2">
-                <div>
-                  {{ item.title }}
-                </div>
-              </div>
+
+              <a :href="`/job/${job.id}`" class="flex-1 cursor-pointer">
+                <!-- Job Title -->
+                <h3 class="text-lg font-semibold text-gray-900">{{ job.title }}</h3>
+
+                <!-- Job Location -->
+                <p class="text-sm text-gray-600">{{ job.candidate_required_location }}</p>
+              </a>
             </div>
-            <div class="px-6 py-4 mb-auto">
-              <div
-                class="font-medium text-lg inline-block hover:text-indigo-600 transition duration-500 ease-in-out mb-2"
-              >
-                {{ item.candidate_required_location }}
-              </div>
-              <div>{{ item.salary }}</div>
+
+            <!-- Spacer to push footer down -->
+            <div class="flex-grow">
             </div>
-            <div class="px-6 py-3 flex flex-row items-center justify-between bg-gray-100">
-              <span
-                href="#"
-                class="py-1 text-xs font-regular text-gray-900 mr-1 flex flex-row items-center"
-              >
-                <span class="font-bold">{{ item.category }}</span>
-              </span>
+
+            <div class="mt-4 flex justify-between items-end text-sm text-gray-700 gap-2">
+              <!-- Job Salary -->
+              <span class="font-medium text-left w-1/2">{{ job.salary.replace(/,000/g, 'K') || 'Unspecified salary' }}</span>
+              <!-- Job Category -->
+              <span class="text-right italic w-1/2">{{ job.category }}</span>
             </div>
-          </a>
+          </div>
         </div>
+
+
+
+
       </div>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, computed } from 'vue'
-import jsonData from '@/assets/remote-jobs.json'
+import { ref, onMounted, computed } from 'vue';
+import jsonData from '@/assets/remote-jobs.json';
+import CountrySelect from '@/components/CountrySelect.vue'
+import { ThumbsUp } from 'lucide-vue-next';
 
 // Variáveis para manter estado sobre: carregamento (loading), erros (error), e os dados obtidos (data).
 const data = ref(null)
@@ -142,14 +106,15 @@ const cachedData = ref(null) // Para evitar acessos repetidos à API (desaconsel
 const searchQuery = ref('')
 const selectedLocation = ref('')
 const selectedSalaryRange = ref('')
+const savedJobs = ref<string[]>([])
 
-// Computed property para filtrar os trabalhos com base na pesquisa.
+// Computed property para filtrar os empregos com base na pesquisa.
 const filteredJobs = computed(() => {
   if (!data.value) return []
   return data.value.jobs.filter((job) => {
     const matchName = job.title.toLowerCase().includes(searchQuery.value.toLowerCase())
     const matchLocation = selectedLocation.value
-      ? job.candidate_required_location == selectedLocation.value
+      ? job.candidate_required_location.split(",").map(c => c.trim()).includes(selectedLocation.value) // candidate_required_location is a comma separated list os countries.
       : true
     let matchSalary = true
 
@@ -193,11 +158,31 @@ const fetchData = async () => {
   }
 }
 
+function toggleSave(jobId: string) {
+  const index = savedJobs.value.indexOf(jobId)
+  if (index === -1) {
+    savedJobs.value.push(jobId)
+  } else {
+    savedJobs.value.splice(index, 1)
+  }
+  localStorage.setItem('savedJobs', JSON.stringify(savedJobs.value))
+}
+
+function isSaved(jobId: string) {
+  return savedJobs.value.includes(jobId)
+}
+
 // Obter dados da API quando a página estiver carregada.
 onMounted(() => {
+  // JSON jobs' data
   data.value = jsonData
   sessionStorage.setItem('remotiveApiData', jsonData) // Too big!!!
   loading.value = false
+
+  // Saved jobs
+  const saved = localStorage.getItem('savedJobs')
+  if (saved) savedJobs.value = JSON.parse(saved)
+
 })
 // onMounted(fetchData);
 </script>
