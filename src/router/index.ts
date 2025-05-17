020555d1af9187a6b1e53ee67fe58daf0701a43b
Carlos Limão <carlos.limao@bitstuff.pt>
2025-05-17 13:57:50 +0100

    Uma página para mostrar empregos preferidos.

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/HomePage.vue' // or your default view
 import About from '../views/AboutPage.vue'
 import Job from '../views/JobItem.vue'
+import SevedJobs from '../views/SavedJobs.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/about', name: 'About', component: About },
   { path: '/job/:id', name: 'Job Details', component: Job },
+  { path: '/saved_jobs', name: 'Saved Jobs', component: SevedJobs },
 ]
 
 const router = createRouter({
